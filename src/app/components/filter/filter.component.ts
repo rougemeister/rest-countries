3e9524e0257093dc7e3fa91c3c80b943b290f390
rougemeister <rougemeister1@gmail.com>
2025-07-03 09:12:57 +0000

    Add

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { setFilterRegion } from '../../store/actions/countries.actions';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
+ regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  selectedRegion: string | null = null;
+  dropdownOpen = false;
 
+  constructor(private store: Store, private eRef: ElementRef) {}
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectRegion(region: string | null) {
+    this.selectedRegion = region;
+    this.dropdownOpen = false;
+    this.store.dispatch(setFilterRegion({ region }));
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      
+      this.dropdownOpen = false;
+    }else {
+      this.selectedRegion = null;
+    }
+  }
 }
